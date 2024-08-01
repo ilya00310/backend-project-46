d776cda688cc30a13a6c638bb4a20b5130638f06
Шихман Илья <109863997+ilya00310@users.noreply.github.com>
2024-08-01 17:08:00 +0500

    Update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
-import getContent from './utils.js'
+import getContent from './utils.js';
+
 const program = new Command();
 
 program
@@ -8,9 +9,9 @@ program
     .description('Compares two configuration files and shows a difference.')
     .version('0.0.1')
     .argument('<filepath1>')
-    // .argument('<filepath2>')
+    .argument('<filepath2>')
     .option('-f, --format <type>', 'output format')
     .action((filepath1, filepath2) => {
-        console.log(getContent(filepath1));
+        console.log(getContent(filepath1, filepath2));
     });
 program.parse();
