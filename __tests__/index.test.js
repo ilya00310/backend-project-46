76f22b64a0bc9210ad2d268bbc3d9b35ac47eb6b
ilya <kotena1980@bk.ru>
2024-08-02 20:14:32 +0500

    change test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -5,7 +5,7 @@ test('two short path', () => {
   expect(getDiff('file1.json', 'file2.json')).toEqual(getAnswer('json-json'));
 });
 test('two deffrent path', () => {
-  expect(getDiff('file1.json', '/home/ilya/backend-project-46/file2.json')).toEqual(getAnswer('json-json'));
+  expect(getDiff('file1.json', '/backend-project-46/file2.json')).toEqual(getAnswer('json-json'));
 });
 test('path do\'nt exist', () => {
   expect(getDiff('file1.json')).toBeUndefined();
