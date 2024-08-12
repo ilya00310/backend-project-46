b9d822deb26f5b8e47aefb25e07828c7955c1a8d
ilya <kotena1980@bk.ru>
2024-08-12 15:11:15 +0500

    change fixture

--- src/logicParser.js
+++ src/logicParser.js
@@ -9,12 +9,12 @@ export default (path) => {
   switch (getExtension(path)) {
     case 'json':
       if (isFullPath(path)) {
-        return doParseJson(fs.readFileSync(join('__fixtures__', path.split('/').pop()), 'utf8'));
+        return doParseJson(fs.readFileSync(join(path.split('/').pop()), 'utf8'));
       }
       return doParseJson(fs.readFileSync(getPath(path, 'utf8')));
     case 'yml':
       if (isFullPath(path)) {
-        return load(fs.readFileSync(join('__fixtures__', path.split('/').pop()), 'utf8'));
+        return load(fs.readFileSync(join(path.split('/').pop()), 'utf8'));
       }
       return load(fs.readFileSync(getPath(path, 'utf8')));
     default: throw new Error('extension don\'t provide');
