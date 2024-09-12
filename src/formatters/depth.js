604f8909d017bf68ddab4d80b915bd9237175af5
ilya <kotena1980@bk.ru>
2024-09-12 19:00:41 +0500

    fix depth.js

--- src/formatters/depth.js
+++ src/formatters/depth.js
@@ -1,50 +1,41 @@
 import _ from 'lodash';
 
 /* eslint-disable import/prefer-default-export */
-const getIndentWithParen = (depth, value, index = undefined, lastIndex = undefined) => {
-  const indent = 4 * depth - 2;
-  if (lastIndex !== undefined) {
-    if (index === lastIndex) {
-      return `${' '.repeat(indent - 2)}}`;
-    }
-    return '';
-  }
-  if (value.status === 'recursion' || value.status === 'unchanged' || !value.status) {
-    return index === 0 ? `{\n${' '.repeat(indent + 1)}` : ' '.repeat(indent + 1);
-  }
-  return index === 0 ? `{\n${' '.repeat(indent)}` : ' '.repeat(indent);
-};
-const doStringify = (value, depth) => {
+const getIndent = (depth, spacesCount = 4) => ' '.repeat(depth * spacesCount - 2);
+
+const doStringify = (value, depth, funcForRecursion = undefined, spacesCount = 4) => {
   if (!_.isObject(value)) {
     return String(value);
   }
-  const lastIndex = Object.entries(value).length - 1;
   const keys = Object.keys(value);
-  const solution = keys.map((currentKey, index) => `${getIndentWithParen(depth, value, index)} ${currentKey}: ${doStringify(value[currentKey], depth + 1)}\n${getIndentWithParen(depth, value, index, lastIndex)}`).join('');
-  return solution;
+  const solution = keys.reduce((acc, currentKey) => {
+    const valueAndKey = { value: value[currentKey], key: currentKey };
+    return funcForRecursion(acc, depth, valueAndKey);
+  }, '');
+  return `{\n${solution}${getIndent(depth - 1, spacesCount)}  }`;
 };
-const getStrByStatus = (acc, depth, value, symbol = '', index = undefined, lastIndex = undefined) => `${acc}${getIndentWithParen(depth, value, index)}${symbol} ${value.key}: ${doStringify(value.value, depth + 1)}\n${getIndentWithParen(depth, value, index, lastIndex)}`;
+const getStrByUnchanged = (acc, depth, value) => `${acc}${getIndent(depth)}  ${value.key}: ${doStringify(value.value, depth + 1, getStrByUnchanged)}\n`;
+const getStrByAdded = (acc, depth, value) => `${acc}${getIndent(depth)}+ ${value.key}: ${doStringify(value.value, depth + 1, getStrByUnchanged)}\n`;
+const getStrByDeleted = (acc, depth, value) => `${acc}${getIndent(depth)}- ${value.key}: ${doStringify(value.value, depth + 1, getStrByUnchanged)}\n`;
+const getStrByChanged = (acc, depth, value) => `${acc}${getIndent(depth)}- ${value.key}: ${doStringify(value.value, depth + 1, getStrByUnchanged)}\n`
+  + `${getIndent(depth)}+ ${value.key}: ${doStringify(value.newValue, depth + 1, getStrByUnchanged)}\n`;
 
 export const stylish = (item) => {
-  const getDiffDepth = (value, depth = 1) => {
-    const lastIndex = Object.entries(value).length - 1;
-    return Object.values(value).reduce((acc, currentValue, currentIndex) => {
-      switch (currentValue.status) {
-        case 'added':
-          return getStrByStatus(acc, depth, currentValue, '+', currentIndex, lastIndex);
-        case 'deleted':
-          return getStrByStatus(acc, depth, currentValue, '-', currentIndex, lastIndex);
-        case 'changed':
-          return `${acc}${getIndentWithParen(depth, currentValue, currentIndex)}- ${currentValue.key}: ${doStringify(currentValue.value, depth + 1)}\n`
-            + `${getIndentWithParen(depth, currentValue)}+ ${currentValue.key}: ${doStringify(currentValue.newValue, depth + 1)}\n${getIndentWithParen(depth, currentValue, currentIndex, lastIndex)}`;
-        case 'unchanged':
-          return getStrByStatus(acc, depth, currentValue, '', currentIndex, lastIndex);
-        case 'recursion':
-          return `${acc}${getIndentWithParen(depth, currentValue, currentIndex)} ${currentValue.key}: ${getDiffDepth(currentValue.value, depth + 1)}\n${getIndentWithParen(depth, currentValue, currentIndex, lastIndex)}`;
-        default:
-          throw new Error('Unexpected status');
-      }
-    }, '');
-  };
-  return getDiffDepth(item);
+  const getDiffDepth = (value, depth = 1) => Object.values(value).reduce((acc, currentValue) => {
+    switch (currentValue.status) {
+      case 'added':
+        return getStrByAdded(acc, depth, currentValue);
+      case 'deleted':
+        return getStrByDeleted(acc, depth, currentValue);
+      case 'changed':
+        return getStrByChanged(acc, depth, currentValue);
+      case 'unchanged':
+        return getStrByUnchanged(acc, depth, currentValue);
+      case 'recursion':
+        return `${acc}${getIndent(depth)}  ${currentValue.key}: {\n${getDiffDepth(currentValue.value, depth + 1)}${getIndent(depth)}  }\n`;
+      default:
+        throw new Error('Unexpected status');
+    }
+  }, '');
+  return `{\n${getDiffDepth(item)}}`;
 };
